4a08f8e22e15d258c3fba3b29c5f6f069b6f89b7
MasonHolland <masontholland@gmail.com>
2017-07-12 09:36:10 -0600

    created test for user story

---
+++ spec/features/user_can_search_by_state_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+# As a user
+# When I visit "/"
+# And I select "Colorado" from the dropdown
+# And I click on "Locate Members from the House"
+# Then my path should be "/search" with "state=CO" in the parameters
+# And I should see a message "7 Results"
+# And I should see a list of 7 the members of the house for Colorado
+# And they should be ordered by seniority from most to least
+# And I should see a name, role, party, and district for each member
+RSpec.feature "searching for house members from a state" do
+  it "returns a list of relevant candidates" do
+    visit '/'
+    select 'Colorado', :from => 'search'
+    click_on "Locate Members from the House"
+
+    expect(current_path).to eq("/search?state=CO")
+    expect(page).to have_content("7 Results")
+  end
+end
