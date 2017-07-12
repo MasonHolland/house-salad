bb20e07b03ba6c07f15dd756e46e19b766c3a6d3
MasonHolland <masontholland@gmail.com>
2017-07-12 09:59:39 -0600

    building out farady functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def index
-    binding.pry
-    
+    conn = Faraday.new(:url => 'https://projects.propublica.org/api-docs/congress-api/endpoints/#get-current-members-by-statedistrict')
+    response = conn.get do |req|
+      req.url '/search'
+      req.headers['API-Key'] = ''
   end
 end
