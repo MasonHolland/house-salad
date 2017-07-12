8952796caca712680946f594fc3d481c6d5447a4
MasonHolland <masontholland@gmail.com>
2017-07-12 11:03:23 -0600

    candidates are sorted

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,7 @@ class SearchController < ApplicationController
     conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{params[:state]}/current.json")
     conn.headers = conn.headers.merge(key)
     response = conn.get '/search'
-    @parsed = JSON.parse(conn.get.body)
-
-    binding.pry
+    parsed = JSON.parse(conn.get.body)
+    @sorted = parsed["results"].sort_by { |peep| peep["seniority"].to_i }
   end
 end
