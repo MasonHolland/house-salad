b4bac8273dbee168fca048281cb9c1d53fa0206c
MasonHolland <masontholland@gmail.com>
2017-07-12 10:35:19 -0600

    passing test as is

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,5 +4,8 @@ class SearchController < ApplicationController
     conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{params[:state]}/current.json")
     conn.headers = conn.headers.merge(key)
     response = conn.get '/search'
+    @parsed = JSON.parse(conn.get.body)
+
+    # binding.pry
   end
 end
